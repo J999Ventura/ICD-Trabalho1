99bf8249186dd2924e38e71efd86bbc71ea724b5
João Filipe Vaz <joaofilipevaz@users.noreply.github.com>
2017-05-21 14:10:55 +0100

    Add files via upload

---
+++ src/commun/Funcionario.java
@@ -0,0 +1,14 @@
+package commun;
+
+public class Funcionario {
+
+    private String userName;
+    private String nomeFuncionario;
+    private String numFuncionario;
+
+    public Funcionario(String userName, String nomeFuncionario, String numFuncionario) {
+        this.userName = userName;
+        this.nomeFuncionario = nomeFuncionario;
+        this.numFuncionario = numFuncionario;
+    }
+}
